1e26e17fb8aad6f20c35f8522af469f4fab877f1
Joseph White <122577026+Joseph-WhiteSWE@users.noreply.github.com>
2023-09-21 17:23:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-const btnEL = document.getElementById("btn-el")
-const inputEl = document.getElementById("input-el")
-const endorseUl = document.getElementById("endorse-el")
- 
+const btnEl = document.getElementById('btn-el');
+const inputEl = document.getElementById('input-el');
+const endorseUl = document.getElementById('endorse-el');
+
 const appSettings = {
-databaseURL: "https://champions-ea7b6-default-rtdb.firebaseio.com/"
-}
+  databaseURL: 'https://champions-ea7b6-default-rtdb.firebaseio.com/',
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const endorseInDB = ref(database, "endorse")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorseInDB = ref(database, 'endorse');
 
-btnEL.addEventListener("click", function () {
-    prependValue(inputEl.value);
-    //to prevent from input from saving to DB
-    // push(endorseInDB, inputEl.value); 
-    clearValue();
+btnEl.addEventListener('click', function () {
+  appendValue(inputEl.value);
+  clearValue();
 });
 
-onValueFunc();
-
-function prependValue(itemValue) {
-    let li = document.createElement("li");
-    li.textContent = itemValue;
-    endorseUl.insertBefore(li, endorseUl.firstChild);
-}
+onValue(endorseInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.values(snapshot.val());
+    endorseUl.innerHTML = '';
+    for (let i = 0; i < itemsArray.length; i++) {
+      appendValue(itemsArray[i]);
+    }
+  }
+});
 
 function clearValue() {
-    inputEl.value = "";
+  inputEl.value = '';
 }
 
-function onValueFunc() {
-    onValue(endorseInDB, function (snapshot) {
-        if (snapshot.exists()) {
-            let itemsArray = Object.values(snapshot.val());
-            endorseUl.innerHTML = "";
-            for (let i = 0; i < itemsArray.length; i++) {
-                prependValue(itemsArray[i]);
-            }
-        }
-    });
+function appendValue(itemValue) {
+  endorseUl.innerHTML += `<li> ${itemValue} </li>`;
 }
-
